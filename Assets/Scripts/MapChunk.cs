9167530eb49ef0412b6e188ee58297ebd9a4b6b7
Boris Shishov <borisshishov@gmail.com>
2019-04-27 14:50:38 +0300

    Seams generation

--- Assets/Scripts/MapChunk.cs
+++ Assets/Scripts/MapChunk.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Assets.Scripts.Utils;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace Assets.Scripts
 {
@@ -23,8 +24,15 @@ namespace Assets.Scripts
         public Bounds LocalSpaceBounds;
         public EnemySpawnSettings[] EnemySpawns;
         public Transform Entry;
+        public Transform EntryA;
+        public Transform EntryB;
         public Transform Exit;
+        public Transform ExitA;
+        public Transform ExitB;
         public AdjacentChunkSettings[] AdjacentChunks;
+        public bool DrawGizmos;
+
+        public int OffLinksCount = 6;
 
         public Vector3 WorldEntryLocation => Entry.position;
         public Vector3 WorldExitLocation => Exit.position;
@@ -70,24 +78,44 @@ namespace Assets.Scripts
             var offset = WorldExitLocation - chunk.WorldEntryLocation;
             chunk.transform.position += offset;
 
+            // Make seams
+            for (var i = 0; i < OffLinksCount; i++)
+            {
+                var k = (float) i / (OffLinksCount - 1);
+                var start = SampleExitLocation(k);
+                var end = chunk.SampleEntryLocation(k);
+                var link = gameObject.AddComponent<NavMeshLink>();
+                link.startPoint = transform.InverseTransformPoint(start);
+                link.endPoint = transform.InverseTransformPoint(end);
+            }
+
             return chunk;
+        }
+
+        public Vector3 SampleEntryLocation(float k)
+        {
+            return Vector3.Lerp(EntryA.position, EntryB.position, k);
+        }
+
+        public Vector3 SampleExitLocation(float k)
+        {
+            return Vector3.Lerp(ExitA.position, ExitB.position, k);
         }
 
         void OnDrawGizmos()
         {
-            // Entry location
-            if (Entry != null)
-            {
-                Gizmos.color = Color.blue;
-                Gizmos.DrawSphere(Entry.position, .2f);
-            }
+            if(!DrawGizmos)
+                return;
 
-            // Exit location
-            if (Exit != null)
-            {
-                Gizmos.color = Color.black;
-                Gizmos.DrawSphere(Exit.position, .2f);
-            }
+            Gizmos.color = Color.cyan;
+
+            // Entry edge
+            if (EntryA != null && EntryB != null)
+                Gizmos.DrawLine(EntryA.position, EntryB.position);
+
+            // Exit edge
+            if (ExitA != null && ExitB != null)
+                Gizmos.DrawLine(ExitA.position, ExitB.position);
             
 
             // Enemy spawns
