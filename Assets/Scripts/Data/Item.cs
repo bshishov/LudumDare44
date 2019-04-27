4ec0d51150a130754df52643be05a67e9457fb3d
DESKTOP-ANE7Q38\User <arfr@live.ru>
2019-04-27 17:30:30 +0200

    Scriptable object added

---
+++ Assets/Scripts/Data/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Data
+{
+    [CreateAssetMenu(fileName = "Item", menuName = "Mechanics/Item")]
+    public class Item : ScriptableObject
+    {
+        public string Name;
+        public int Cost;
+
+        [SerializeField]
+        public Buff[] buffs;
+    }
+}
