209fd984ee5f89edafa730b21d9fe05de927f8a7
Boris Shishov <borisshishov@gmail.com>
2019-04-29 12:49:51 +0300

    Shop

--- Assets/Scripts/Data/Item.cs
+++ Assets/Scripts/Data/Item.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Assets.Scripts.Data
 {
@@ -9,8 +7,11 @@ namespace Assets.Scripts.Data
     {
         public string Name;
         public int Cost;
+        public Sprite Icon;
 
         [SerializeField]
         public Buff[] Buffs;
+
+        public GameObject Prefab;
     }
 }
