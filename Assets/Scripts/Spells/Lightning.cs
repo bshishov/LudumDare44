6d0db2f895cf31e8e725ef81e21cb4548c4bdbc9
Boris Shishov <borisshishov@gmail.com>
2019-04-29 13:43:39 +0300

    Lightning Spell Effect and LightningFX separate

---
+++ Assets/Scripts/Spells/Lightning.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Spells
+{
+    [RequireComponent(typeof(LineRenderer))]
+    public class Lightning : MonoBehaviour
+    {
+        public int Points = 10;
+        private LineRenderer _renderer;
+
+        public void Start()
+        {
+            _renderer = GetComponent<LineRenderer>();
+            Destroy(gameObject, 1f);
+        }
+
+        public void SetupLine(Vector3 from, Vector3 to)
+        {
+            _renderer = GetComponent<LineRenderer>();
+            var positions = new Vector3[Points];
+            for (var i = 0; i < Points; i++)
+            {
+                positions[i] = Vector3.Lerp(from, to, i / (Points - 1f));
+            }
+
+            _renderer.positionCount = Points;
+            _renderer.SetPositions(positions);
+        }
+
+        public void SetupLine(Transform from, Transform to)
+        {
+            SetupLine(from.position, to.position);
+        }
+
+        public void SetupDefaultDebug()
+        {
+            SetupLine(transform.position, transform.position + Vector3.forward * 5f);
+        }
+    }
+}
--- Assets/Scripts/Spells/LightningSpellEffect.cs
+++ Assets/Scripts/Spells/LightningSpellEffect.cs
@@ -3,18 +3,9 @@ using UnityEngine;
 
 namespace Assets.Scripts.Spells
 {
-    [RequireComponent(typeof(LineRenderer))]
     public class LightningSpellEffect : MonoBehaviour, ISpellEffect
     {
-        public int Points = 10;
-        private LineRenderer _renderer;
-
-        public void Start()
-        {
-            _renderer = GetComponent<LineRenderer>();
-            //SetupLine(transform.position, transform.position + Vector3.forward * 5f);
-            Destroy(gameObject, 10f);
-        }
+        public GameObject LightningPrefab;
 
         public void OnSpellStateChange(Spell spell, ContextState newState)
         {
@@ -33,27 +24,12 @@ namespace Assets.Scripts.Spells
             {
                 foreach (var dst in tgt.destinations)
                 {
-                    SetupLine(tgt.source.transform.position, dst.transform.position);
+                    var lObj = GameObject.Instantiate(LightningPrefab, transform);
+                    lObj.GetComponent<Lightning>().SetupLine(tgt.source.transform.position, dst.transform.position);
                 }
             }
-            
         }
 
-        public void SetupLine(Vector3 from, Vector3 to)
-        {
-            var positions = new Vector3[Points];
-            for (var i = 0; i < Points; i++)
-            {
-                positions[i] = Vector3.Lerp(from, to, i / (Points - 1f));
-            }
-
-            _renderer.positionCount = Points;
-            _renderer.SetPositions(positions);
-        }
-
-        public void SetupLine(Transform from, Transform to)
-        {
-            SetupLine(from.position, to.position);
-        }
+        
     }
 }
