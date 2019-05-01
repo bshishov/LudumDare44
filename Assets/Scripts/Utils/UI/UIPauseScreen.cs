757f87c720cf3c2927cdcccdcaa3a6a130de8fcb
DESKTOP-ANE7Q38\User <arfr@live.ru>
2019-05-01 15:32:40 +0200

    Hide spellpanel

--- Assets/Scripts/Utils/UI/UIPauseScreen.cs
+++ Assets/Scripts/Utils/UI/UIPauseScreen.cs
@@ -6,6 +6,7 @@ namespace Assets.Scripts.Utils.UI
     public class UIPauseScreen : MonoBehaviour
     {
         private UICanvasGroupFader _fader;
+        public GameObject CastPanel;
 
         void Start()
         {
@@ -16,10 +17,16 @@ namespace Assets.Scripts.Utils.UI
         private void StateChanged()
         {
             if (_fader.State == UICanvasGroupFader.FaderState.FadedIn)
+            {
                 Time.timeScale = 0f;
+                CastPanel.SetActive(false);
+            }
 
             if (_fader.State == UICanvasGroupFader.FaderState.FadedOut)
+            {
                 Time.timeScale = 1f;
+                CastPanel.SetActive(true);
+            }
         }
 
         void Update()
