9ec6b8f86da33c083264425f2074bd943930d552
DESKTOP-ANE7Q38\User <arfr@live.ru>
2019-05-02 15:42:27 +0200

    Kinda buffer

--- Assets/Scripts/Tags.cs
+++ Assets/Scripts/Tags.cs
@@ -6,6 +6,8 @@
         public static string MapChunk = "MapChunk";
         public static string Shop = "Shop";
         public static string Interactable = "Interactable";
+        public static string Enemy = "Enemy";
+        public static string Buffer = "Buffer";
     }
 }
 
