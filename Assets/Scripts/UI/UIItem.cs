bc6513302bce8312332540fdd0e88bbc37387283
Boris Shishov <borisshishov@gmail.com>
2019-05-03 00:28:13 +0300

    Materials

---
+++ Assets/Scripts/UI/UIItem.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Scripts.Data;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIItem : MonoBehaviour
+{
+    public Image Icon;
+    public TextMeshProUGUI StacksText;
+
+    private Item _item;
+    
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
