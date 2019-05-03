bf4c18b577c6b3cc287a55bb3a6e9ab528673a11
Boris Shishov <borisshishov@gmail.com>
2019-05-03 03:33:31 +0300

    Item bar

---
+++ Assets/Scripts/UI/UIItemsBar.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts;
+using Assets.Scripts.Data;
+using UnityEngine;
+
+public class UIItemsBar : MonoBehaviour
+{
+    public GameObject UIItemPrefab;
+
+    private CharacterState _playerState;
+    private readonly List<UIItem> _items = new List<UIItem>();
+
+    void Start()
+    {
+        var player = GameObject.FindGameObjectWithTag(Tags.Player);
+        if (player != null)
+        {
+            _playerState = player.GetComponent<CharacterState>();
+            if (_playerState == null)
+            {
+                Debug.LogWarning("PlayerState not found");
+            }
+        }
+
+        if (_playerState != null)
+        {
+
+            _playerState.OnItemPickup += PlayerStateOnOnItemPickup;
+            
+        }
+    }
+
+    void UpdateItems()
+    {
+        foreach (var itemState in _playerState.Items)
+        {
+            var existing = _items.FirstOrDefault(i => i.Item.Equals(itemState.Item));
+            if (existing != null)
+            {
+                existing.Setup(itemState.Item, itemState.Stacks);
+            }
+            else
+            {
+                var newItem = Instantiate(UIItemPrefab, transform, false).GetComponent<UIItem>();
+                newItem.Setup(itemState.Item, itemState.Stacks);
+                _items.Add(newItem);
+            }
+        }
+    }
+
+    private void PlayerStateOnOnItemPickup(Item item, int stacks)
+    {
+        UpdateItems();
+    }
+}
