dddc1474aebc7643059c3ba2f26e58030d35ba9d
Gladky Dmitry <miktor.ru@gmail.com>
2019-05-06 00:01:45 +0300

    fix spellbook state serialization

---
+++ Assets/Scripts/Spells/ISpellCastListener.cs
@@ -0,0 +1,12 @@
+﻿using Assets.Scripts.Data;
+
+namespace Spells
+{
+public interface ISpellCastListener
+{
+    void OnAbortedtFiring(Spell spell);
+    void OnStartFiring(Spell    spell);
+    void OnEndFiring(Spell      spell);
+    void OnEndCastinng(Spell    spell);
+}
+}
--- Assets/Scripts/Spells/SpellbookState.cs
+++ Assets/Scripts/Spells/SpellbookState.cs
@@ -5,14 +5,6 @@ using UnityEngine.Assertions.Must;
 
 namespace Spells
 {
-public interface ISpellCastListener
-{
-    void OnAbortedtFiring(Spell spell);
-    void OnStartFiring(Spell    spell);
-    void OnEndFiring(Spell      spell);
-    void OnEndCastinng(Spell    spell);
-}
-
 [RequireComponent(typeof(SpellCaster))]
     public abstract class SpellbookState : MonoBehaviour, ISpellCastListener
     {
