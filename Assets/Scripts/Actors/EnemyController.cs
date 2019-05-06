43ee773fdec50df63223c66fce2111cd58f8eab4
Gladky Dmitry <miktor.ru@gmail.com>
2019-05-07 02:14:29 +0300

    fix troll targeting a little

--- Assets/Scripts/Actors/EnemyController.cs
+++ Assets/Scripts/Actors/EnemyController.cs
@@ -158,23 +158,20 @@ public class EnemyController : MonoBehaviour
 
     private void CastAttack(CharacterState player, Vector3 len, float distance, int spellCount)
     {
+        _movement.LookAt(player.transform.position);
+
         if (distance > _spellRange)
         {
             _movement.SetDestination(player.transform.position);
-            _movement.LookAt(player.transform.position);
         }
         else
         {
-            if (distance > _fearRange)
+            if (_characterState.CanDealDamage())
             {
-                if (_characterState.CanDealDamage())
-                {
-                    _movement.LookAt(player.transform.position);
-                    _spellbookState.TryFireSpellToTarget(Mathf.FloorToInt(Random.value * spellCount), player, null);
-                    _movement.Stop();
-                }
+                _spellbookState.TryFireSpellToTarget(Mathf.FloorToInt(Random.value * spellCount), player, null);
+                _movement.Stop();
             }
-            else
+            else if (distance < _fearRange)
             {
                 var tgt = transform.position - _fearRange * len.normalized;
                 _movement.SetDestination(tgt);
