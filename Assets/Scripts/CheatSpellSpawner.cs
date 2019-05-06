ed0e3350d05664c1d81110012275bcf338ffc995
Boris Shishov <borisshishov@gmail.com>
2019-05-06 12:17:49 +0300

    Mechanics test scene

---
+++ Assets/Scripts/CheatSpellSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Actors;
+using Assets.Scripts.Data;
+using UnityEngine;
+
+public class CheatSpellSpawner : MonoBehaviour
+{
+    public GameObject Prefab;
+    public float Padding = 1f;
+    public int ItemsInRow = 10;
+    private Cheats _cheats;
+
+    void Start()
+    {
+        _cheats = FindObjectOfType<Cheats>();
+        var idx = 0;
+        foreach (var cheatsSpell in _cheats.Spells)
+        {
+            var col = idx % ItemsInRow;
+            var row = idx / ItemsInRow;
+            var pos = new Vector3(col, 0, row) * Padding;
+
+
+            var go = GameObject.Instantiate(Prefab, transform.position + pos, Quaternion.identity);
+            if (go != null)
+            {
+                var dSpell = go.GetComponent<DroppedSpell>();
+                if (dSpell != null)
+                {
+                    dSpell.Setup(cheatsSpell, 1);
+                }
+                go.transform.SetParent(transform, true);
+                idx++;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Dummy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterState))]
+public class Dummy : MonoBehaviour
+{
+    public TextMeshPro Text;
+
+    private CharacterState _characterState;
+    private StringBuilder _stringBuilder = new StringBuilder();
+
+    void Start()
+    {
+        _characterState = GetComponent<CharacterState>();
+    }
+    
+    void Update()
+    {
+        _stringBuilder.AppendFormat("HP: {0}/{1}\n", _characterState.Health, _characterState.MaxHealth);
+        _stringBuilder.AppendFormat("SPD: {0:##}\n", _characterState.Speed);
+
+        foreach (var b in _characterState.Buffs)
+        {
+            _stringBuilder.AppendFormat("{0} <color=red>x{1}</color>: <color=yellow>{2:0.#}s</color> \n", b.Buff.name, b.Stacks, b.TimeRemaining);
+        }
+
+        Text.text = _stringBuilder.ToString();
+        _stringBuilder.Clear();
+    }
+}
