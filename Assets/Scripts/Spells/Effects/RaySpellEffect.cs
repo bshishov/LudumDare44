9657bc4a3eb34cdc7c6d14355c5a50822fef0e4c
Boris Shishov <borisshishov@gmail.com>
2019-05-08 02:05:39 +0300

    Rage ray 0.1

---
+++ Assets/Scripts/Spells/Effects/RaySpellEffect.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Spells.Effects
+{
+    public class RaySpellEffect : MonoBehaviour, ISubSpellEffect
+    {
+        private struct SubSpellEffectInstance
+        {
+            public GameObject InstanceObject;
+            public IRayEffect Ray;
+            public TargetInfo Source;
+        }
+
+        public GameObject RayPrefab;
+        public bool AutoDestroyAfterSpell = true;
+
+        private readonly Dictionary<ISpellContext, SubSpellEffectInstance> _instances 
+            = new Dictionary<ISpellContext, SubSpellEffectInstance>(1);
+
+        void Start()
+        {
+        }
+        
+        void Update()
+        {
+            foreach (var kvp in _instances)
+            {
+                var context = kvp.Key;
+                var instance = kvp.Value;
+
+                var position = context.ChannelingInfo.GetNewTarget()?.Position;
+                if (position != null)
+                    instance.Ray.RayUpdated(instance.Source.Transform.position,
+                        position.Value);
+            }
+        }
+
+        public void OnTargetsPreSelected(ISpellContext context, SpellTargets targets)
+        {
+            if (_instances.ContainsKey(context))
+                return;
+            
+            
+            var go = Instantiate(RayPrefab, transform, true);
+            if (go != null)
+            {
+                var ray = go.GetComponent<IRayEffect>();
+
+                if(ray == null)
+                {
+                    Debug.LogWarning("Created a ray instance but it has no IRayEffect");
+                    Destroy(go);
+                    return;
+                }
+
+                var instance = new SubSpellEffectInstance
+                {
+                    Ray = ray,
+                    InstanceObject = go,
+                    Source = targets.Source
+                };
+                _instances.Add(context, instance);
+
+                // Raise started event
+                var position = context.ChannelingInfo?.GetNewTarget()?.Position;
+                if (position != null)
+                    ray.RayStarted(targets.Source.Transform.position,
+                        position.Value);
+            }
+        }
+
+        public void OnTargetsAffected(ISpellContext context, SpellTargets targets)
+        {
+        }
+
+        public void OnEndSubSpell(ISpellContext context)
+        {
+            if (_instances.TryGetValue(context, out var instance))
+            {
+                instance.Ray.RayEnded();
+
+                if (AutoDestroyAfterSpell)
+                    Destroy(instance.InstanceObject);
+
+                _instances.Remove(context);
+            }
+        }
+    }
+}
