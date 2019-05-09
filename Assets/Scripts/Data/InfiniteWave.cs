8ccbaade788386e783c6062630f416a8cfb4bcdc
Boris Shishov <borisshishov@gmail.com>
2019-05-09 12:20:37 +0300

    Arena and waves

---
+++ Assets/Scripts/Data/InfiniteWave.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Data
+{
+    [Serializable]
+    public class InfiniteWaveEntry
+    {
+        public SpawnEntry[] Items;
+        public float DelayBeforeNextPack;
+        public int Difficulty;
+        public int MaxNumber = 5;
+
+        public int TotalNumberOfEnemies(int spawnPointsCount, int difficulty)
+        {
+            if (Difficulty >= difficulty)
+                return 0;
+
+            var multiplier = Mathf.Min(difficulty - Difficulty, MaxNumber);
+            var total = Items.Sum(e => e.GetTotal(spawnPointsCount));
+            return total * multiplier;
+        }
+    }
+
+    [CreateAssetMenu(fileName = "infinite_wave", menuName = "Gameplay/Infinite Wave")]
+    public class InfiniteWave : ScriptableObject
+    {
+        public List<InfiniteWaveEntry> Packs;
+
+        public int TotalNumberOfEnemies(int spawnPointsCount, int difficulty)
+        {
+            return Packs.Sum(p => p.TotalNumberOfEnemies(spawnPointsCount, difficulty));
+        }
+    }
+}
---
+++ Assets/Scripts/Data/Wave.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Data
+{
+    [CreateAssetMenu(fileName = "wave", menuName = "Gameplay/Wave")]
+    public class Wave : ScriptableObject
+    {
+        public List<SpawnEntry> Items;
+
+        public int TotalNumberOfEnemies(int spawnPoints)
+        {
+            return Items.Sum(spawnEntry => spawnEntry.GetTotal(spawnPoints));
+        }
+    }
+}
