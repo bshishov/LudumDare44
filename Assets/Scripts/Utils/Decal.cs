8b8563dc00ed346ce2bd5ac6753fd7dbee0a1504
Boris Shishov <borisshishov@gmail.com>
2019-05-15 19:03:29 +0300

    Decals

---
+++ Assets/Scripts/Utils/Decal.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(MeshRenderer))]
+    public class Decal : MonoBehaviour
+    {
+        private MeshRenderer _renderer;
+        private Camera _camera;
+        
+        void Start()
+        {
+            _renderer = GetComponent<MeshRenderer>();
+            _camera = Camera.main;
+        }
+        
+        void Update()
+        {
+            var cam = _camera;
+            var m = _renderer.localToWorldMatrix;
+            var v = cam.worldToCameraMatrix;
+            var p = GL.GetGPUProjectionMatrix(cam.projectionMatrix, false);
+            var mvp = p * v * m;
+            _renderer.sharedMaterial.SetMatrix("_invMVP", mvp.inverse);
+        }
+    }
+}
